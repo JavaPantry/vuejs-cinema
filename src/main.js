96ef6a596628124d05f026a291b685ed72d8a8be
Alexei Ptitchkin <ptitchkin@gmail.com>
2018-12-23 11:12:03 -0500

    69. Setup the movie-list component

--- src/main.js
+++ src/main.js
@@ -4,10 +4,25 @@ new Vue({
 	el: '#app',
 	components: {
 		'movie-list': {
-			template: `<div id="movie-list"> movie list </div>`
+			template: `<div id="movie-list">
+							<div v-for="movie in movies" class="movie">
+								{{movie.title}}
+							</div> 
+                       </div>`,
+			data() {
+				return{
+				movies: [
+					{title:'Pulp Fiction'},
+					{title:'Home alone'},
+					{title:'God Father I'},
+					{title:'God Father II'},
+				]}
+			}
 		},
 		'movie-filter': {
-			template: `<div id="movie-filter">movie filter</div>`
+			template: `<div id="movie-filter"> 
+						<h2>filter results</h2>
+					   </div>`
 		}
 	},
 	data:{}
