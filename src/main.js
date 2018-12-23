acd11f82af0d3fa8b1212711569c527a5d1f0184
Alexei Ptitchkin <ptitchkin@gmail.com>
2018-12-23 13:09:56 -0500

    73. Making check-filter toggleable

--- src/main.js
+++ src/main.js
@@ -36,7 +36,12 @@ new Vue({
 					   </div>`,
 			components: {
 				'check-filter':{
-					template: `<div class="check-filter">
+					data(){
+						return{
+							checked: false
+						}
+					},
+					template: `<div v-bind:class="{'check-filter':true, active:checked}"  v-on:click="checked = !checked">
 									<span class="checkbox"></span>
 									<span class="check-filter-title">{{title}}</span>
 							   </div>`,
