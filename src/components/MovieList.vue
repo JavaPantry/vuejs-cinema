94fcb166f291c6ce1788961f4a7d77c9344a3407
Alexei Ptitchkin <ptitchkin@gmail.com>
2018-12-25 15:06:37 -0500

    87. Fleshing out movie-item template

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,13 +1,7 @@
 <template>
-	<!--<div id="movie-list">
-		<div v-for="movie in filteredMovies" class="movie">
-			{{movie.movie.Title}}
-		</div>
-	</div>-->
 	<div id="movie-list">
 		<movie-item v-for="movie in filteredMovies"
-		            class="movie"
-		            v-bind:movie="movie">
+		            v-bind:movie="movie.movie">
 		</movie-item>
 	</div>
 
@@ -29,19 +23,6 @@
 				return this.movies.filter(this.moviePassesGenreFilter)
 			}
 		},
-		/*data() {
-			return{
-				movies: [
-					{title:'Pulp Fiction', genre: genres.CRIME},
-					{title:'Home alone', genre: genres.COMEDY},
-					{title:'Austin Powers', genre: genres.COMEDY},
-					{title:'God Father I', genre: genres.CRIME},
-					{title:'God Father II', genre: genres.CRIME},
-					{title:'Blue Planet', genre: genres.DOCUMENTARY},
-					{title:'DRAMA Movie', genre: genres.DRAMA},
-					{title:'HORROR Movie', genre: genres.HORROR}
-				]}
-		},*/
 		props:['genre', 'time', 'movies'],
 		components: {
 			MovieItem
