4880004f1ba5d8ca9d1f761fdff9c06376704a7f
Alexei Ptitchkin <ptitchkin@gmail.com>
2018-12-27 11:03:12 -0500

    98. Using a global event bus in the project 97. Vue essentials: Event bus

--- src/main.js
+++ src/main.js
@@ -11,6 +11,11 @@ import moment from 'moment-timezone';
 moment.tz.setDefault("UTC");
 Object.defineProperty(Vue.prototype, '$moment', { get() { return this.$root.moment } });
 
+import { checkFilter } from './util/bus';
+const bus = new Vue();
+Object.defineProperty(Vue.prototype, '$bus', { get() { return this.$root.bus } });
+
+
 new Vue({
 	el: '#app',
 	data(){
@@ -19,20 +24,8 @@ new Vue({
 			time:[],
 			movies: [],
 			moment,
-            day: moment()
-		}
-	},
-	methods: {
-		checkFilter( category, title, checked){
-			console.log('Root on checkFilter event' , category, title, checked)
-			if(checked){
-				this[category].push(title)
-			}else{
-				let index = this[category].indexOf(title);
-				if (index === -1)
-					return
-				this[category].splice(index,1);
-			}
+            day: moment(),
+			bus
 		}
 	},
 	components: {
@@ -43,5 +36,6 @@ new Vue({
 		this.$http.get('/api').then(response => {
 			this.movies = response.data;
 		});
+        this.$bus.$on('check-filter', checkFilter.bind(this));
 	}
 })
